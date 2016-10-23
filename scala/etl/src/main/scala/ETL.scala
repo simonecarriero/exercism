668cb76eb6f1f04ade787b43d91731b7c2bee25e
Simone Carriero <simonecarriero@gmail.com>
2016-10-23 19:12:30 +0200

    Add scala etl iteration 2

--- scala/etl/src/main/scala/ETL.scala
+++ scala/etl/src/main/scala/ETL.scala
@@ -2,8 +2,8 @@ object ETL {
 
   def transform(scores: Map[Int, Seq[String]]): Map[String, Int] = {
     for {
-      score <- scores
-      value <- score._2
-    } yield (value.toLowerCase, score._1)
+      (points, values) <- scores
+      value <- values
+    } yield (value.toLowerCase, points)
   }
 }
